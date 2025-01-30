197545ee250e9cdc042999b5fea7cfc06a769a93
IfritSorcerer <ianisern@yahoo.com>
2025-01-30 11:58:26 -0500

    Step 2: word_count function

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 
-
-
-
 def main():
-    with open("books/frankenstein.txt") as f:
+    book_path = "books/frankenstein.txt"
+
+    with open(book_path) as f:
+        file_contents.lower()
         file_contents = f.read()
-    
+
+    print(f"Lets have a look at {book_path}")
+    print(f"The total number of words present in the document is: {word_count(file_contents)}")
+
+
+    print("Thus concludes my book report")
+
+def word_count(file_contents):
+    word_count = file_contents.split()
+    return len(word_count)    
+
+
+
+
+
 
 main()
