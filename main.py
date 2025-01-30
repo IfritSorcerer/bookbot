94f1bfb3813723dc469aff10d72a69f8b5e539bb
IfritSorcerer <ianisern@yahoo.com>
2025-01-30 12:32:53 -0500

    Step 3: character counting dictionary

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
-
 def main():
+    #The path used to read the file 
     book_path = "books/frankenstein.txt"
 
+    #The following with block will read the text from the book_path variable
     with open(book_path) as f:
-        file_contents.lower()
         file_contents = f.read()
 
-    print(f"Lets have a look at {book_path}")
+    print(f"--- Lets have a look at {book_path} ---")
     print(f"The total number of words present in the document is: {word_count(file_contents)}")
 
+    print(f"{char_count(file_contents)}")
+    print(f"--- Thus concludes my book report ---")
 
-    print("Thus concludes my book report")
 
 def word_count(file_contents):
     word_count = file_contents.split()
     return len(word_count)    
 
+def char_count(file_contents):
+    char_count = {}
+    for char in file_contents:
+        char = char.lower()
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+    
 
 
 
